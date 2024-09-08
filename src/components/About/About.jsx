3e9c12007adcaa303ef4b70b622a82ea90396c55
osmankelifa <145119027+osmankelifa@users.noreply.github.com>
2024-09-08 20:40:39 +0200

    took away pic of sennai

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,7 @@ const About = () => {
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
           <div data-aos="slide-right" data-aos-duration="1500">
-            <img
-              src={sennai}
-              alt=""
-              className="sm:scale-125 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]"
-            />
+            
           </div>
           <div>
             <div className="space-y-5 sm:p-16 pb-6">
