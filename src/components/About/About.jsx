0f022d29eb348355bc19088a8a2c3ba4dfeed67d
osmankelifa <145119027+osmankelifa@users.noreply.github.com>
2024-11-19 20:28:43 +0100

    om oss fade up removed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
               >
                 Om oss
               </h1>
-              <p data-aos="fade-up" className="leading-8 tracking-wide">
+              <p className="leading-8 tracking-wide">
             
 Vår historia
 Habesha Beer föds ur Etiopiens hjärta, bryggd med förstklassiga ingredienser och ett engagemang för kvalitet. Vi är stolta över att ta med denna autentiska smak till Sverige och knyta samman kulturer en klunk i taget.
