587d7f9aea3954ee06b445c651fe889103977186
osmankelifa <145119027+osmankelifa@users.noreply.github.com>
2024-12-14 22:25:39 +0100

    responsive contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
               Finns på utvalda restauranger, klubbar och återförsäljare. Vill du sälja Habesha Beer?
               </p>
             </div>
-            <div className="sm:grid sm:place-items-center">
+            <div className="flex justify-center sm:place-items-center">
               <a
                 href="mailto:sennai@hotmail.com"
                 className="inline-block font-semibold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 "
